b61fb6eb00c2fc267527520e8ef6091a1b69918d
NelMacc <nelsonwkmac@gmail.com>
2024-02-09 16:09:23 -0800

    Added an array with envelope objects

---
+++ server/envelopes.js
@@ -0,0 +1,21 @@
+let envelopes = [
+  {
+    id: 1,
+    category: "Groceries",
+    budget: 200,
+  },
+  {
+    id: 2,
+    category: "Bills",
+    budget: 1000,
+  },
+  {
+    id: 3,
+    category: "Entertainment",
+    budget: 200,
+  },
+];
+
+envelopes[0].budget = envelopes[0].budget - 100
+
+console.log(envelopes[0].budget)
