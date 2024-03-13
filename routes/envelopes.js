48311cd1f1e4c7bc898b7440c6536abd01435b5b
NelMacc <nelsonwkmac@gmail.com>
2024-03-12 17:24:14 -0700

    Added a .post request to create an envelope with error handling

--- routes/envelopes.js
+++ routes/envelopes.js
@@ -1,40 +1,40 @@
 const express = require("express");
 const app = express();
 
-let envelopes = require('../database/db')
-
 const envelopesRouter = express.Router();
+envelopesRouter.use(express.json());
 module.exports = envelopesRouter
 
-envelopesRouter.use(express.json());
-
-/* const newEnvelope = {
-  id: 4,
-  category: "Gas",
-  budget: 300,
-}; 
-
-envelopes.push(newEnvelope) */
+let envelopes = require('../database/db')
 
 // retrieves all envelopes
 envelopesRouter.get("/envelopes", (req, res) => {
   res.send(envelopes);
 }); 
 
+// retrieves one envelope by id
 envelopesRouter.get("/envelopes/:id", (req, res) => {
   const envelope = envelopes.find(e => e.id === parseInt(req.params.id));
   if (!envelope) res.status(404).send('The envelope with that ID was not found.');
   res.send(envelope);
 });
 
-envelopesRouter.post('/envelopes', (req, res) => {
-  if (!req.body.category || !typeof req.body.category === 'string') {
-    res.status(400).send('Category should be a string')
-  }
+// checks if only letters a through z are used
+const isAlpha = str => /^[A-Z]+$/i.test(str);
+// checks if only numbers are used
+const isNumber = n => /^-?[\d.]+(?:e-?\d+)?$/.test(n)
 
-  if (!req.body.budget || !typeof req.body.budget === 'number') {
-    res.status(400).send('Budget should be a number')
-  }
+// creates a new envelope
+envelopesRouter.post('/envelopes', (req, res) => {
+  if (!req.body.category || !isAlpha(req.body.category)) {
+    res.status(400).send('Category should only use letters');
+    return;
+  };
+
+  if (!req.body.budget || !isNumber(req.body.budget)) {
+    res.status(400).send('Budget should be a number');
+    return;
+  };
   
   const envelope = {
     id: envelopes.length + 1,
